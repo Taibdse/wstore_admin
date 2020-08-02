8696e2473a6fbbbbb48a7bb0be54a98725023124
Taibdse <buiductai232@gmail.com>
2020-08-02 11:45:05 +0700

    add content size to editor

--- src/components/common/MyEditor.vue
+++ src/components/common/MyEditor.vue
@@ -1,10 +1,16 @@
 <template>
-  <vue-editor v-model="myContent" text-change="handleTextChange" style="width: 100%"></vue-editor>
+    <div>
+        <small style="float: right">Size: {{sizeContent}} kb</small>
+        <div style="clear: both"></div>
+        <vue-editor v-model="myContent" text-change="handleTextChange" style="width: 100%"></vue-editor>
+
+    </div>
 </template>
 
 <script>
 
 import { VueEditor, Quill } from "vue2-editor";
+import { getByteSize } from '../../utils/strings';
 
 export default {
     props: {
@@ -16,17 +22,20 @@ export default {
     },
     
     data: () => ({
-        myContent: ''
+        myContent: '',
+        sizeContent: 0
     }),
 
     watch: {
         content: function(newVal, oldval){
             this.myContent = newVal;
+            this.sizeContent = getByteSize(newVal);
         }
     },
     
     created(){
         this.myContent = this.$props.content;
+        this.sizeContent = getByteSize(this.myContent);
     } 
 }
 </script>
