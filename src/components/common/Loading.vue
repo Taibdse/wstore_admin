99698d17dc51e1bad68f364cfec0ede9a4b79fbd
taibdse <buiductai232@gmail.com>
2021-04-14 00:09:10 +0700

    enable full page backdrop

--- src/components/common/Loading.vue
+++ src/components/common/Loading.vue
@@ -3,7 +3,7 @@
       :active="isLoading"
       :can-cancel="false"
       :on-cancel="onCancel"
-      :is-full-page="false"
+      :is-full-page="true"
       loader="spinner"
       width="50"
       height="50"
