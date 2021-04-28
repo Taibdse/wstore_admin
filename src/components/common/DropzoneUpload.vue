8d3c1b6cdc1dd3e5b27ac3bde718eba5fcc79e37
taibdse <buiductai232@gmail.com>
2021-04-28 14:42:14 +0700

    fix can not render image Dropzone

--- src/components/common/DropzoneUpload.vue
+++ src/components/common/DropzoneUpload.vue
@@ -1,100 +1,100 @@
 <template>
- <div>
-    <vue-dropzone 
-        ref="dropzone" 
-        :id="id" 
-        :options="dropzoneOptions"
-        :include-styling="true"
-        :vdropzone-sending="sendingEvent"
-        :vdropzone-removed-file="handleRemoveFile"
-        :duplicateCheck="true"
-        :vdropzone-canceled="handleCancelFile"
-        >
-        <div class="dropzone-custom-content">
-            <h3 class="dropzone-custom-title">Drag and drop to upload content!</h3>
-            <div class="subtitle">...or click to select a file from your computer</div>
+  <div>
+    <vue-dropzone
+      ref="dropzone"
+      :id="id"
+      :options="dropzoneOptions"
+      :include-styling="true"
+      :vdropzone-sending="sendingEvent"
+      :vdropzone-removed-file="handleRemoveFile"
+      :duplicateCheck="true"
+      :vdropzone-canceled="handleCancelFile"
+    >
+      <div class="dropzone-custom-content">
+        <h3 class="dropzone-custom-title">Drag and drop to upload content!</h3>
+        <div class="subtitle">
+          ...or click to select a file from your computer
         </div>
+      </div>
     </vue-dropzone>
     <!-- <button @click="getFiles">get files</button> -->
- </div>
+  </div>
 </template>
 
 <script>
-import vue2Dropzone from 'vue2-dropzone';
-import 'vue2-dropzone/dist/vue2Dropzone.min.css';
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 
 export default {
   components: {
-    vueDropzone: vue2Dropzone
+    vueDropzone: vue2Dropzone,
   },
   props: {
-      id: String,
-      uploadMultiple: Boolean,
-      uploadedFiles: Array
+    id: String,
+    uploadMultiple: Boolean,
+    uploadedFiles: Array,
   },
 
-  created(){
-    
-  },
+  created() {},
 
-  mounted(){
+  mounted() {
     this.showUploadedFilesOnDropzone();
   },
 
   data: function () {
     return {
       dropzoneOptions: {
-          url: 'https://httpbin.org/post',
-          thumbnailWidth: 150,
-          maxFilesize: 0.5,
-          headers: { "My-Awesome-Header": "header value" },
-          addRemoveLinks: true,
-          dictDefaultMessage: 'Upload here',
-          uploadMultiple: this.$props.uploadMultiple,
-          maxFiles: this.$props.uploadMultiple ? 10 : 1, 
-          thumbnailMethod: 'crop',
-          acceptedMimeTypes: 'image/*',
-      }
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        headers: { "My-Awesome-Header": "header value" },
+        addRemoveLinks: true,
+        dictDefaultMessage: "Upload here",
+        uploadMultiple: this.$props.uploadMultiple,
+        maxFiles: this.$props.uploadMultiple ? 10 : 1,
+        thumbnailMethod: "crop",
+        acceptedMimeTypes: "image/*",
+      },
+    };
+  },
+
+  watch: {
+    uploadedFiles: function() {
+      this.showUploadedFilesOnDropzone();
     }
   },
-  
+
   methods: {
-        showUploadedFilesOnDropzone: function(){
-            this.uploadedFiles.forEach(file => {
-                const options = { size: 123, name: file.name, type: "image/png" };
-                this.$refs.dropzone.manuallyAddFile(options, file.url);
-            })
-        },
-        
-        removeAllFiles() {
-            this.$refs.dropzone.removeAllFiles();
-        },
-        
-        sendingEvent: function(){
-        },
+    showUploadedFilesOnDropzone: function () {
+      this.uploadedFiles.forEach((file) => {
+        const options = { size: 123, name: file.name, type: "image/png" };
+        this.$refs.dropzone.manuallyAddFile(options, file.url);
+      });
+    },
 
-        handleRemoveFile: function(file, error, xhr){
-        
-        },
+    removeAllFiles() {
+      this.$refs.dropzone.removeAllFiles();
+    },
 
-        handleCancelFile: function(file){
-        },
+    sendingEvent: function () {},
 
-        getAllFiles: function(){
-            return this.$refs.dropzone.dropzone.files
-        },
+    handleRemoveFile: function (file, error, xhr) {},
 
-        getManuallyAddedFiles: function(){
-            return this.getAllFiles().filter(file => file.manuallyAdded);
-        },
+    handleCancelFile: function (file) {},
 
-        getUploadedFiles: function(){
-            return this.getAllFiles().filter(file => !file.manuallyAdded);
-        }
-        
-  }
+    getAllFiles: function () {
+      return this.$refs.dropzone.dropzone.files;
+    },
 
-}
+    getManuallyAddedFiles: function () {
+      return this.getAllFiles().filter((file) => file.manuallyAdded);
+    },
+
+    getUploadedFiles: function () {
+      return this.getAllFiles().filter((file) => !file.manuallyAdded);
+    },
+  },
+};
 </script>
 
 <style>
